a9d90f9817801e92365f3054ec74597af3a4f39b
github username <dekadekadeka@gmail.com>
2019-08-13 22:45:46 -0400

    stocks

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,27 +5,86 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state={
+    allStocks:[],
+    myStocks:[],
+    filteredStocks:[]
+  }
+
+componentDidMount(){
+  fetch("http://localhost:3000/stocks")
+  .then(resp => resp.json())
+  .then(data => {
+    this.setState({allStocks: data,
+    filteredStocks: data})
+  })
+}
+
+buyStock = (stockHash) =>{
+  let myStockArray = [...this.state.myStocks, stockHash]
+  this.setState({myStocks: myStockArray})
+}
+
+sellStock = (stockHash) => {
+  let soldStocks = this.state.myStocks.filter(stock => stock !==  stockHash)
+  this.setState({myStocks: soldStocks})
+}
+
+handleSort = (e) => {
+  if(e.target.value === "All"){
+    this.setState({filteredStocks: this.state.allStocks})
+  }else{
+
+  let filteredStocksArray = this.state.allStocks.filter(stock =>{
+    if(stock.type === e.target.value) return true
+    else return false
+  })
+  this.setState({filteredStocks: filteredStocksArray})
+}
+}
+
+handleRadioButtons=(e) => {
+  if(e.target.value === "Alphabetically"){
+    let filteredStocksArray = this.state.allStocks.sort((a, b) => {
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
+    return 0;})
+    this.setState({filteredStocks: filteredStocksArray})
+    
+  }else if (e.target.value === "Price"){
+    let filteredStocksArray = this.state.allStocks.sort((a, b) => a.price - b.price)
+    this.setState({filteredStocks: filteredStocksArray})
+  }
+}
+
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleSort={this.handleSort} handleRadioButtons={this.handleRadioButtons}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer
+              allStocks={this.state.filteredStocks}
+              buyStock={this.buyStock}
+              />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer
+              myStocks={this.state.myStocks}
+              sellStock={this.sellStock}
+              />
 
             </div>
           </div>
       </div>
     );
   }
-
 }
+
 
 export default MainContainer;
--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -9,6 +9,9 @@ class PortfolioContainer extends Component {
         <h2>My Portfolio</h2>
           {
             //render your portfolio stocks here
+            this.props.myStocks.map(myStocks=> 
+            <Stock stock={myStocks}
+          handleClick={this.props.sellStock}/>)
           }
       </div>
     );
